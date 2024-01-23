1d31051d86a418d3dab3c0d81d55e5cc2ca6bb75
Jxy <865526875@qq.com>
2024-01-23 15:00:36 +0800

    Change __init__.py

--- gdbi/__init__.py
+++ gdbi/__init__.py
@@ -1,6 +1,6 @@
 from .node_export_config import NodeExportConfig
 from .edge_export_config import EdgeExportConfig
-from .neo4j.neo4jInterface import Neo4jInterface
+from .neo4j import Neo4jInterface
 __all__ = [
     'NodeExportConfig',
     'EdgeExportConfig',
