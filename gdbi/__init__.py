0946456b1f8423f3217b38e466fb08585863f0c6
jxy <865526875@qq.com>
2024-01-31 10:36:09 +0800

    Remove gstore interface

--- gdbi/__init__.py
+++ gdbi/__init__.py
@@ -3,16 +3,12 @@ from .edge_export_config import EdgeExportConfig
 from .neo4j import Neo4jInterface
 from .nebula import NebulaInterface
 from .atlas import AtlasInterface
-from .GstoreConnector import GstoreConnector
-from .gstore import GstoreInterface
 __all__ = [
     'NodeExportConfig',
     'EdgeExportConfig',
     'Neo4jInterface',
     'NebulaInterface',
-    'AtlasInterface',
-    'GstoreConnector',
-    'GstoreInterface'
+    'AtlasInterface'
 ]
 
 classes = __all__
