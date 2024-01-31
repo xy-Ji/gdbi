f9d87b000fe9a4cf7913da4b7a337d22a2132ffb
jxy <865526875@qq.com>
2024-01-31 12:23:19 +0800

    Add gstore interface

--- gdbi/__init__.py
+++ gdbi/__init__.py
@@ -3,12 +3,16 @@ from .edge_export_config import EdgeExportConfig
 from .neo4j import Neo4jInterface
 from .nebula import NebulaInterface
 from .atlas import AtlasInterface
+from .GstoreConnector import GstoreConnector
+from .gstore import GstoreInterface
 __all__ = [
     'NodeExportConfig',
     'EdgeExportConfig',
     'Neo4jInterface',
     'NebulaInterface',
-    'AtlasInterface'
+    'AtlasInterface',
+    'GstoreConnector',
+    'GstoreInterface'
 ]
 
 classes = __all__
