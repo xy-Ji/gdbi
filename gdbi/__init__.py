d89789bf75ea694ab6cee48ab5d35041bd7ad42f
Jxy <865526875@qq.com>
2024-02-01 19:32:00 +0800

    update __init__.py

--- gdbi/__init__.py
+++ gdbi/__init__.py
@@ -2,7 +2,6 @@ from .node_export_config import NodeExportConfig
 from .edge_export_config import EdgeExportConfig
 from .neo4j import Neo4jInterface
 from .nebula import NebulaInterface
-from .atlas import AtlasInterface
 from .GstoreConnector import GstoreConnector
 from .gstore import GstoreInterface
 __all__ = [
@@ -10,7 +9,6 @@ __all__ = [
     'EdgeExportConfig',
     'Neo4jInterface',
     'NebulaInterface',
-    'AtlasInterface',
     'GstoreConnector',
     'GstoreInterface'
 ]
